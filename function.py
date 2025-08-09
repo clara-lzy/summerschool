87b217704435521fdf7dbf0f08eedbfca6361097
clara-lzy <ziyang726lele@Gmail.com>
2025-08-09 21:39:02 +0800

    change

--- function.py
+++ function.py
@@ -2,11 +2,30 @@ import numpy as np
 from scipy import signal
 
 def crcbgenqcsig(dataX, snr, qcCoefs):
-    # Generate a quadratic chirp signal
-
+    """
+    Generate a quadratic chirp signal
+    
+    Parameters:
+    -----------
+    dataX : array-like
+        Vector of time stamps at which the samples of the signal are to be computed
+    snr : float
+        Matched filtering signal-to-noise ratio
+    qcCoefs : array-like
+        Vector of three coefficients [a1, a2, a3] that parametrize the phase 
+        of the signal: a1*t + a2*t^2 + a3*t^3
+    
+    Returns:
+    --------
+    sigVec : numpy.ndarray
+        
+    """
     # Convert inputs to numpy arrays for element-wise operations
     dataX = np.array(dataX)
     qcCoefs = np.array(qcCoefs)
+
+    # print('dataX:', dataX)
+    # print('qcCoefs:', qcCoefs)
     
     # Calculate phase vector: a1*t + a2*t^2 + a3*t^3
     phaseVec = qcCoefs[0] * dataX + qcCoefs[1] * dataX**2 + qcCoefs[2] * dataX**3
@@ -19,8 +38,27 @@ def crcbgenqcsig(dataX, snr, qcCoefs):
     
     return sigVec
 
+
 def statgaussnoisegen(n_samples, psd_vals, fltr_order, samp_freq):
-    #Generate a realization of stationary Gaussian noise with given 2-sided PSD
+    """
+    Generate a realization of stationary Gaussian noise with given 2-sided PSD
+    
+    Parameters:
+    n_samples : int
+        Number of samples to generate
+    psd_vals : array-like, shape (M, 2)
+        Matrix containing frequencies and corresponding PSD values
+        First column: frequencies, Second column: PSD values
+        Frequencies must start from 0 and end at Fs/2
+    fltr_order : int
+        Order of the FIR filter to be used
+    samp_freq : float
+        Sampling frequency
+        
+    Returns:
+    out_noise : ndarray
+        Generated colored noise realization
+    """
     
     # Design FIR filter with T(f) = square root of target PSD
     freq_vec = psd_vals[:, 0]
@@ -36,7 +74,6 @@ def statgaussnoisegen(n_samples, psd_vals, fltr_order, samp_freq):
     # Pass through the designed filter and scale
     out_noise = np.sqrt(samp_freq) * signal.fftconvolve(in_noise,b, mode='same')
     return out_noise
-
 
 
 def innerprod_psd(x_vec, y_vec, samp_freq, psd_vals):
@@ -57,7 +94,7 @@ def innerprod_psd(x_vec, y_vec, samp_freq, psd_vals):
     samp_freq : float
         Sampling frequency of X and Y
     psd_vals : array_like
-        PSD values at positive DFT frequencies
+        PSD values at positive DFT frequencies[0,fs/2]
     
     Returns:
     --------
@@ -80,10 +117,7 @@ def innerprod_psd(x_vec, y_vec, samp_freq, psd_vals):
     if len(y_vec) != n_samples:
         raise ValueError('Vectors must be of the same length')
 
-    k_nyq = n_samples // 2 +1
-
-    # print('k_nyq:', k_nyq)
-    # print('psd_vals length:', len(psd_vals))  
+    k_nyq = n_samples // 2 +1 
 
     if len(psd_vals) != k_nyq:
         raise ValueError('PSD values must be specified at positive DFT frequencies')
@@ -95,6 +129,7 @@ def innerprod_psd(x_vec, y_vec, samp_freq, psd_vals):
     # for negative frequencies
     neg_f_strt = 1 - (n_samples % 2) #even~1,odd~0
     psd_vec_4_norm = np.concatenate([psd_vals, psd_vals[( (k_nyq-1) - neg_f_strt):0:-1]])
+
     
     data_len = samp_freq * n_samples #duration of the signal in seconds
     inn_prod = (1 / data_len) * np.sum((fft_x / psd_vec_4_norm) * np.conj(fft_y))
@@ -116,7 +151,7 @@ def normsig4psd(sig_vec, samp_freq, psd_vec, snr):
     samp_freq : float
         Sampling frequency
     psd_vec : array_like
-        Noise PSD vector. Should be specified at the positive DFT frequencies 
+        Noise PSD vector. Should be specified at the positive DFT frequencies[0, fs/2]
         corresponding to the length of sig_vec and sampling frequency samp_freq
     snr : float
         Desired signal to noise ratio
@@ -149,6 +184,7 @@ def normsig4psd(sig_vec, samp_freq, psd_vec, snr):
     
     # Norm of signal squared is inner product of signal with itself
     norm_sig_sqrd = innerprod_psd(sig_vec, sig_vec, samp_freq, psd_vec)
+    # print('norm_sig_sqrd:', norm_sig_sqrd)
     
     # Normalization factor
     norm_fac = snr / np.sqrt(norm_sig_sqrd)
@@ -161,9 +197,15 @@ def normsig4psd(sig_vec, samp_freq, psd_vec, snr):
 
 
 def glrtqcsig(time_vec,data_vec, fs,psd_vec,qcCoefs):
+    '''
 
-    "calculate the GLRT for a quadratic chirp signal with unknown amplitude"
-    'psd_vec: for positive DFT frequencies'
+    calculate the GLRT for a quadratic chirp signal with unknown amplitude
+
+    psd_vec: for positive DFT frequencies[0, fs/2]
+
+    qcCoefs : array-like Vector of three coefficients [a1, a2, a3] that parametrize the phase of the quadratic chirp signal: a1*t + a2*t^2 + a3*t^3
+    
+    '''
     
     # Generate unit norm template
     sig_vec = crcbgenqcsig(time_vec, 1, qcCoefs)
